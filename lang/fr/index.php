afdcfe94d8b557dcd25078c25577b39dae1f61f1
Benjamin V <benjamin.voisin@ens-rennes.fr>
2024-01-29 19:02:58 +0100

    🩹

--- lang/fr/index.php
+++ lang/fr/index.php
@@ -1,5 +1,5 @@
 <?php
   $lang_shortdesctipionname = 'Présentation'; 
   $lang_shortdesctipion = "Bienvenu sur ma page personnelle ! Je suis actuellement étudiant en deuxième année (M1 SIF) à l'ENS de Rennes";
-  $lang_cv_link = "cv/CV_french.pdf"
+  $lang_cv_link = "cv/CV_français.pdf"
 ?>
