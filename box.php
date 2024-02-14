dc631197c5ad07f28249a265f3c5a16f4db02850
Benjamin Voisin <bvois319@darwin.eleves.ens-rennes.fr>
2024-02-14 14:02:12 +0100

    fix

--- box.php
+++ box.php
@@ -16,8 +16,11 @@ if ($lang === 'en') {
 <div class="conteneur">
 
       <span style="position: relative; float: left;">
-      <a style="text-decoration:none" href="https://perso.eleves.ens-rennes.fr/people/alexis.hamon/crocodile.html" class="bouton1"><?php echo $previous ?></a>
+		<?php echo '<a style="text-decoration:none" href="' . urlLanguage('projets.php', $lang) . '" class="bouton1">' . $previous . '</a>'; ?>
       </span>
+	<br>
+	<br>
+	<br>
 <h3><?php echo $input ?></h3>
         <?php echo '<input type="text" id="input" placeholder="' . $input_text . '">'; ?>
         <button id="btn"><?php echo $button_text ?></button>
@@ -31,10 +34,9 @@ const btn= document.getElementById("btn");
 btn.addEventListener('click', function(){
   var dna = document.getElementById("input").value;
   resultdiv = document.getElementById("output");
-  ptr =Calculer les mots absents minimaux stringToNewUTF8(dna);
+  ptr = stringToNewUTF8(dna);
   result = _foisdeux(ptr);
   result_to_js = UTF8ToString(result);
-  console.log(result_to_js);
   resultdiv.innerHTML = result_to_js;
 
 });
