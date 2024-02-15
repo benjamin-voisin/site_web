92efffe44a9ce4d1d517f7c32e94416ade967647
Benjamin V <benjamin.voisin@ens-rennes.fr>
2024-02-15 15:01:33 +0100

    ✨💄 more beutifull and add a .dot output

--- box.php
+++ box.php
@@ -16,30 +16,96 @@ if ($lang === 'en') {
 <div class="conteneur">
 
       <span style="position: relative; float: left;">
-		<?php echo '<a style="text-decoration:none" href="' . urlLanguage('projets.php', $lang) . '" class="bouton1">' . $previous . '</a>'; ?>
+
+        <div class="alert">
+          <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+          <span>&#9888;</span> 
+          <?php echo $waring ?>
+        </div> 
+
+    <?php echo '<a style="text-decoration:none" href="' . urlLanguage('projets.php', $lang) . '" class="bouton1">' . $previous . '</a>'; ?>
       </span>
-	<br>
-	<br>
-	<br>
+  <br>
+  <br>
+  <br>
 <h3><?php echo $input ?></h3>
         <?php echo '<input type="text" id="input" placeholder="' . $input_text . '">'; ?>
         <button id="btn"><?php echo $button_text ?></button>
+
         <h3><?php echo $output ?></h3>
+        <div class="flex-container">
+          <div class="flex-child 1">
+            <h4><?php echo $maw_title ?></h4>
+            <center>
+              <div class="scroll">
+                <button class="copyBtn" id="copyAw">
+                  <?php echo $copy ?>
+                </button>
+                <div id="awoutput"></div>
+              </div>
+            </center>
+          </div>
+          <div class="flex-child 2">
+            <h4><?php echo $dot_title ?></h4>
+            <center>
+              <div class="scroll">
+                <button class="copyBtn" id="copyDot">
+                  <?php echo $copy ?>
+                </button>
+                <div id="dotoutput"></div>
+              </div>
+            </center>
+          </div>
+        </div>
+
         <div id="output"></div>
         <script src="box.js"></script>
 <script>
+const btn = document.getElementById("btn");
+const copyAwBtn = document.getElementById("copyAw");
+const copyDotBtn = document.getElementById("copyDot");
 
-const btn= document.getElementById("btn");
-
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function () {
   var dna = document.getElementById("input").value;
-  resultdiv = document.getElementById("output");
+  awoutput = document.getElementById("awoutput");
+  dotoutput = document.getElementById("dotoutput");
   ptr = stringToNewUTF8(dna);
-  result = _foisdeux(ptr);
-  result_to_js = UTF8ToString(result);
-  resultdiv.innerHTML = result_to_js;
+  arbre = _buildST(ptr);
+  absentWords = _computeAbsentWords(arbre);
 
+  dot = _computeDOT(arbre);
+  dotString = UTF8ToString(dot);
+  absentWordString = UTF8ToString(absentWords);
+  awoutput.innerHTML = absentWordString;
+  dotoutput.innerHTML = dotString;
 });
+
+copyAwBtn.addEventListener('click', function () {
+  copyToClipboard("awoutput", copyAwBtn);
+});
+
+copyDotBtn.addEventListener('click', function () {
+  copyToClipboard("dotoutput", copyDotBtn);
+});
+
+function copyToClipboard(elementId, button) {
+  const element = document.getElementById(elementId);
+  const text = element.innerText;
+
+  navigator.clipboard.writeText(text)
+    .then (() => {
+    button.innerText = "Result Copied";
+    setTimeout(() => {
+    button.innerText = "Copy to clipboard";
+    }, 2000);
+  })
+    .catch(err => {
+    button.innerText = "The text has not been copied, an error occured";
+    setTimeout(() => {
+    button.innerText = "Copy to clipboard";
+    }, 2000);
+    });
+}
 </script>
 
         <hr>
--- lang/en/box.php
+++ lang/en/box.php
@@ -4,4 +4,8 @@
   $input_text = 'Enter DNA string';
   $button_text = 'Compute Minimal Absent Words';
   $previous = 'Back';
+  $warning = 'For very large DNA strings, the memory consumed by this page can be very large. Expect a x500 ratio (a 10MB string needs 5GB of RAM).<br>For large dataset, use our CLI tool available here : <a href="https://framagit.org/AlexisHamon/box_project" target="_blank">framagit.org/AlexisHamon/box_project</a>';
+  $maw_title = 'Minimal Absent Words';
+  $copy = 'Copy to clipboard';
+  $dot_title = 'Suffix tree (in .dot format)';
 ?>
