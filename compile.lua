b42a0321d6a8880fa1028710fe2a31365b00e37d
Benjamin Voisin <benjamin.voisin@ens-rennes.fr>
2024-12-22 18:30:25 +0100

    ✨ now compile.lua automatically find files to compile

--- compile.lua
+++ compile.lua
@@ -4,10 +4,11 @@ local function compile_source(source)
 	local base_langage = "en"
 	local other_langages = { "fr" }
 
-	local F = require(source)
+	local F = require(source.require_path)
 	if (F.build) then
-		os.execute(string.format("mkdir -p ./public/"))
-		local target = io.open(string.format("./public/%s.html", source), "w")
+		os.execute(string.format("mkdir -p ./public/%s", source.directory))
+		os.execute(string.format("cp ./style.css ./public/"))
+		local target = io.open(string.format("./public/%s.html", source.require_path), "w")
 		if (not target) then
 			io.stderr:write("Could not open result file\n")
 			os.exit(1)
@@ -15,8 +16,9 @@ local function compile_source(source)
 		target:write(F:content(base_langage))
 		target:close()
 		for _, langue in pairs(other_langages) do
-			os.execute(string.format("mkdir -p ./public/%s", langue))
-			target = io.open(string.format("./public/%s/%s.html", langue, source), "w")
+			os.execute(string.format("mkdir -p ./public/%s/%s", langue, source.directory))
+			os.execute(string.format("cp ./style.css ./public/%s", langue))
+			target = io.open(string.format("./public/%s/%s.html", langue, source.require_path), "w")
 			if (not target) then
 				io.stderr:write("Could not open result file\n")
 				os.exit(1)
@@ -30,7 +32,25 @@ local function compile_source(source)
 	end
 end
 
-local sources = { "index", "recherche/index", "recherche/stagel3", "recherche/projetM1" }
-for _, source in pairs(sources) do
+local function get_sources()
+	local result = {}
+	local find, err = io.popen("find . -name '*.lua'")
+	if (not find) then
+		io.stderr:write(err)
+		os.exit(1)
+	end
+	local file = find:read("*l")
+	while (file) do
+		local require_path = string.gsub(file, ".lua", "")
+		local directory = string.gsub(require_path, "/[%w]-$", "")
+		if (require_path ~= "./compile" and require(require_path).build) then
+			table.insert(result, { require_path = require_path, directory = directory })
+		end
+		file = find:read("*l")
+	end
+	return result
+end
+
+for _, source in pairs(get_sources()) do
 	compile_source(source)
 end
