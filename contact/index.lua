4676875edb221d7c614417e26a82aab671fd5155
Benjamin Voisin <benjamin.voisin@ens-rennes.fr>
2024-12-23 11:35:12 +0100

    Add contact info

--- contact/index.lua
+++ contact/index.lua
@@ -1,10 +1,32 @@
 local Base = require("base")
 
+local para = {
+	fr = [[
+  Vous pouvez me contacter à mon adresse mail suivante : <a id="melcontact" title="to send me an email"> <font color="blue"> prenom.nom@ens-rennes.fr </font> </a>. Ma clé PGP est disponible <a href="../media/public_key.asc" download>ici</a>.
+]],
+	en = [[
+  You can email me at <a id="melcontact" title="to send me an email"> <font color="blue"> firstname.name@ens-rennes.fr </font> </a>. My PGP key is available <a href="../media/public_key.asc" download>here</a>.
+]],
+}
+
+local script = [[
+
+<script>
+  function mail() {
+    let a = "YmVuamFtaW4udm9pc2lu";
+    let b = "QGVucy1yZW5uZXMuZnI";
+    mel = atob(a+b);
+    meldiv = document.getElementById("melcontact");
+    meldiv.innerHTML = mel;
+    meldiv.href = atob("bWFpbHRvOg==") + mel;
+  }
+  mail();
+</script>
+]]
 return {
 	build = true,
 	lien = "contact/index.html",
 	content = function(self, langue)
-		local content = "Contact..."
-		return Base.base(langue, content, "Contact", "../style.css", 1, self.lien)
+		return Base.base(langue, para[langue] .. script, "Contact", "../style.css", 1, self.lien)
 	end
 }
