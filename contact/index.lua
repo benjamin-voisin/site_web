fcdadedcf90c3673b92c904f903e672c35a7501c
Benjamin Voisin <benjamin.voisin@ens-rennes.fr>
2024-12-24 16:26:48 +0100

    📝 better contact info

--- contact/index.lua
+++ contact/index.lua
@@ -2,30 +2,34 @@ local Base = require("base")
 
 local para = {
 	fr = [[
-  Vous pouvez me contacter à mon adresse mail suivante : <a id="melcontact" a="YmVuamFtaW4udm9pc2lu" b="QGVucy1yZW5uZXMuZnI">prenom.nom@ens-rennes.fr</a>. Ma clé PGP est disponible <a href="../media/public_key.asc" download>ici</a>.
+<h2>Mail (méthode à privilégier)</h2>
+Pour les contacts professionnels, le meilleur moyen de contact reste  les mails, de préférence chiffré (avec ma clé <a href="../media/public_key.asc" download>PGP</a> <font size=2>(F6BB 7666 62E7 D433 F7EE 159E C5C8 EDD1 25FB FD78)</font>).
+<h2>Messageries instantanées</h2>
+Je suis également joignables sur les messagerise instantannées (par ordre de préférence) :
+<br><br>
+<table>
+  <tr><td>XMPP</td><td>benjaminv@xmpp-dev.sinfonie.org</td></tr>
+  <tr><td>Discord</td><td>@bvoisin</td></tr>
+  <tr><td>Mastodon</td><td><a href="https://piaille.fr/@benjaminV">@benjaminV@piaille.fr</a></td></tr>
+</table>
 ]],
 	en = [[
-  You can email me at <a id="melcontact" a="YmVuamFtaW4udm9pc2lu" b="QGVucy1yZW5uZXMuZnI">firstname.name@ens-rennes.fr</a>. My PGP key is available <a href="../media/public_key.asc" download>here</a>.
+<h2>Email (preferred method)</h2>
+For professional contacts, the preffered means still is email, encrypted if possible (with my <a href="../media/public_key.asc" download>PGP key</a> <font size=2>(F6BB 7666 62E7 D433 F7EE 159E C5C8 EDD1 25FB FD78)</font>).
+<h2>Instant Messaging Systems</h2>
+I’m also available on theese instant messaging systems (ordered by preferences) :
+<table>
+  <tr><td>XMPP</td><td>benjaminv@xmpp-dev.sinfonie.org</td></tr>
+  <tr><td>Discord</td><td>@bvoisin</td></tr>
+  <tr><td>Mastodon</td><td><a href="https://piaille.fr/@benjaminV">@benjaminV@piaille.fr</a></td></tr>
+</table>
 ]],
 }
 
-local script = [[
-<script>
-  function courrielcontact() {
-    let courrieldiv = document.getElementById("melcontact");
-    let a = courrieldiv.getAttribute("a");
-    let b = courrieldiv.getAttribute("b");
-    let courriel = atob(a+b);
-    courrieldiv.innerHTML = courriel;
-    courrieldiv.href = atob("bWFpbHRvOg==") + courriel;
-  }
-  eval(atob("c2V0VGltZW91dCgoKSA9PiBjb3VycmllbGNvbnRhY3QoKSwgMTAwMCk="));
-</script>
-]]
 return {
 	build = true,
 	lien = "contact/index.html",
 	content = function(self, langue)
-		return Base.base(langue, para[langue] .. script, "Contact", "../style.css", 1, self.lien)
+		return Base.base(langue, para[langue], "Contact", "../style.css", 1, self.lien)
 	end
 }
